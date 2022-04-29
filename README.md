fdc4c89cb601c6b7eca0b4de6c811cd0306a5c17
TiffMonique <tiffanymatamoros72@gmail.com>
2022-04-28 19:10:00 -0600

    Page 1 finished

--- README.md
+++ README.md
@@ -1,1 +1,24 @@
-# graphql-nextjs-rick---Morty-wiki
+# Rick and Morty Graphql App
+
+<img src="https://i0.wp.com/hipertextual.com/wp-content/uploads/2020/06/hipertextual-rick-y-morty-cuarta-temporada-mejor-mezcla-humor-negro-y-ciencia-ficcion-todo-multiverso-2020420540.jpg?fit=1500%2C844&ssl=1" />
+
+## Installation
+
+Install all the needed packages:
+
+ ```
+ npm i
+ ```
+
+
+
+## Usage
+
+To run the app:
+
+ ```
+
+ ```
+
+npm run dev
+ ```
---
+++ src/components/Character.js
@@ -0,0 +1,24 @@
+import React from "react";
+import Image from "next/image";
+import { Heading, Text, SimpleGrid, Box } from "@chakra-ui/react";
+
+const Character = ({ characters }) => {
+  return (
+    <SimpleGrid columns={[1, 2, 3]} spacing="40px">
+      {characters.map((character) => {
+        return (
+          <Box bg="#00ff01" key={character.id}>
+            <Image src={character.image} width={300} height={300} />
+            <Heading as="h4" align="center" size="md">
+              {character.name}
+            </Heading>
+            <Text align="center">Origin: {character.origin.name}</Text>
+            <Text align="center">Location: {character.location.name}</Text>
+          </Box>
+        );
+      })}
+    </SimpleGrid>
+  );
+};
+
+export default Character;
